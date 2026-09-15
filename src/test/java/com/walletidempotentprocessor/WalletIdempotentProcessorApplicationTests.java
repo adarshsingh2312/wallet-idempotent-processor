6028696b927bd120f8b88db8dad07ee81d542b61
Adarsh Singh <adarshsinghraj01@gmail.com>
2026-09-15 18:06:26 +0530

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/walletidempotentprocessor/WalletIdempotentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletidempotentprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletIdempotentProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
